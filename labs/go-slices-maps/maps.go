b1e5be61853272478176d3df25fbdc5e7f2ca40e
Santiago Juarez Badillo Chavez <santiago10.jbch@gmail.com>
2020-01-29 16:43:41 -0600

    init

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,25 @@
 package main
 
-func main() {
+import (
+	"golang.org/x/tour/wc"
+	"fmt"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+
+	var splitedString = strings.Split(string(s)," ")
+	var words map[string]int
+	words = map[string]int{} 
+	for _, current := range splitedString {
+		words[current]++
+		fmt.Printf(string(words[current]))
+	}
+	return words
 }
+
+func main() {
+	wc.Test(WordCount)
+}
+
+
--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -1,4 +1,20 @@
 package main
 
-func main() {
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	var image = make([][]uint8, dy)
+	for y := 0; y < dy; y++ {
+		image[y] = make([]uint8, dx)
+		for x := 0; x < dx; x++ {
+			image[y][x] = uint8(x*y)
+		}
+	}
+	return image[:dy]
 }
+
+func main() {
+	pic.Show(Pic)
+}
+
+
