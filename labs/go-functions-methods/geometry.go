f6a91b5278ef72537da726c4a5cd9b45d0a9aa9e
Santiago Juarez Badillo Chavez <santiago10.jbch@gmail.com>
2020-02-03 13:22:51 -0600

    Solve

--- labs/go-functions-methods/geometry.go
+++ labs/go-functions-methods/geometry.go
@@ -1,29 +1,47 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+package main
 
-// See page 156.
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"os"
+	"strconv"
+)
 
-// Package geometry defines simple types for plane geometry.
-//!+point
-package geometry
+func main() {
+	sides := os.Args[1]
+	nOfPoints, err := strconv.Atoi(sides)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("- Generating a [%d] sides figure\n", nOfPoints)
+	points := make(Path, nOfPoints)
+	fmt.Println("- Figure's verices")
+	for i := 0; i < nOfPoints; i++ {
+		point := Point{rand.Float64() * 100, rand.Float64() * 100}
+		fmt.Printf("   - ( %f, %f )\n", point.X(), point.Y())
+		points[i] = point
+	}
 
-import "math"
-
-type Point struct{ X, Y float64 }
-
-// traditional function
-func Distance(p, q Point) float64 {
-	return math.Hypot(q.X-p.X, q.Y-p.Y)
+	fmt.Println("- Figure's Perimeter")
+	fmt.Printf("   -")
+	distance := points.Distance()
+	fmt.Printf(" = %f\n", distance)
 }
+
+type Point struct{ x, y float64 }
 
 // same thing, but as a method of the Point type
 func (p Point) Distance(q Point) float64 {
-	return math.Hypot(q.X-p.X, q.Y-p.Y)
+	return math.Hypot(q.X()-p.X(), q.Y()-p.Y())
 }
 
-//!-point
-
-//!+path
+func (p Point) X() float64 {
+	return p.x
+}
+func (p Point) Y() float64 {
+	return p.y
+}
 
 // A Path is a journey connecting the points with straight lines.
 type Path []Point
@@ -33,10 +51,16 @@ func (path Path) Distance() float64 {
 	sum := 0.0
 	for i := range path {
 		if i > 0 {
-			sum += path[i-1].Distance(path[i])
+
+			tempDistance := path[i-1].Distance(path[i])
+			if i < len(path)-1 {
+				fmt.Printf(" %f +", tempDistance)
+			} else {
+				fmt.Printf(" %f ", tempDistance)
+			}
+
+			sum += tempDistance
 		}
 	}
 	return sum
 }
-
-//!-path
